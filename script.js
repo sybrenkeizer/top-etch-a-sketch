da179c900c320ac173780e9ac51146d24a9a017d
Sybren <92291687+sybrenkeizer@users.noreply.github.com>
2022-05-09 12:54:18 +0530

    Restructure script and fix functions
    - Make essential functions work with new layout
    - Organized script in sensable fashion

--- script.js
+++ script.js
@@ -1,139 +1,147 @@
 // Gather elements
 const gridContainer = document.querySelector('.grid__container');
 const customColorInput = document.querySelector('.custom-color__input');
+const customColorRadio = document.querySelector('.custom-color__radio');
 const canvasColorInput = document.querySelector('.canvas-color__input');
 const randomColorInput = document.querySelector('.random-color__input');
+const darkGradientInput = document.querySelector('.dark-gradient__input');
+const lightGradientInput = document.querySelector('.light-gradient__input');
 const eraserBtn = document.querySelector('.eraser__input');
-const clearFieldBtn = document.querySelector('.clear-field__btn');
+const clearCanvasBtn = document.querySelector('.clear-field__btn');
 const gridSizerDescription = document.querySelector('.grid-sizer__description');
-const gridSizerInput = document.querySelector('.grid-sizer__input');
-const showGridBtn = document.querySelector('.show-grid-btn');
+const gridInputRange = document.querySelector('.grid-sizer__input');
+const showGridBtn = document.querySelector('.toggle-grid__input');
 const saveBtn = document.querySelector('.save-btn');
 const buttons = document.querySelectorAll('.btn');
+const customColorDescription = document.querySelector('.custom-color__description');
+const canvasColorDescription = document.querySelector('.canvas-color__description');
+const drawRadioBoxes = document.querySelectorAll('.draw-radio');
+const radioDescriptions = document.querySelectorAll('.radio__description');
+const canvasColorRadio = document.querySelector('.canvas-color__radio');
 
 
 window.onload = function() {
-  setGrid(48);
+  setGrid(currentSize);
 }
 
-// Store setting mouse down and mouse up
+
+// Default options
+const DEFAULT_OPTION = 'customColor';
+const DEFAULT_SIZE = 50;
+
+
+// Main settings
+let currentDrawOption = DEFAULT_OPTION;
+let currentSize = DEFAULT_SIZE;
 let mouseIsDown = false;
+
+
+// Main functions
+function setCurrentDrawOption(newOption) {
+  currentDrawOption = newOption;
+}
+function setCurrentSize(newSize) {
+  currentSize = newSize;
+}
+
+
+// Store interactions
+customColorInput.onclick = () => setCurrentDrawOption('customColor');
+customColorDescription.onclick = () => setCurrentDrawOption('customColor')
+randomColorInput.onclick = () => setCurrentDrawOption('randomColor');
+darkGradientInput.onclick = () => setCurrentDrawOption('darkGradient');
+lightGradientInput.onclick = () => setCurrentDrawOption('lightGradient');
+eraserBtn.onclick = () => setCurrentDrawOption('eraser');
 document.body.onmousedown = () => (mouseIsDown = true);
 document.body.onmouseup = () => (mouseIsDown = false);
 
 
-// Activate/deactivate button random colors
-let randomColorInputActive = false;
-randomColorInput.addEventListener('click', function() {
-  randomColorInput.classList.toggle('btn-active');
-  if (randomColorInputActive === false) {
-    randomColorInputActive = true;
-  } else if (randomColorInputActive === true) {
-    randomColorInputActive = false;
+// Drawing function
+function draw(event) {
+  if (event.type === 'mouseover' && !mouseIsDown) return
+  switch(currentDrawOption) {
+    case 'customColor':
+      customColor();
+      break;
+    case 'randomColor':
+      randomColor();
+      break;
+    case 'darkGradient':
+      darkGradient();
+      break;
+    case 'lightGradien':
+      lightGradient();
+      break;
+    case 'eraser':
+      eraser();
+      break;
   }
-})
+}
 
 
-// Activate/deactivate button eraser
-let eraserBtnActive = false;
-eraserBtn.addEventListener('click', function() {
-  eraserBtn.classList.toggle('btn-active');
-  if (eraserBtnActive === false) {
-    eraserBtnActive = true;
-  } else if (eraserBtnActive === true) {
-    eraserBtnActive = false
-  }
-})
+// Drawing options
+function customColor() {
+  event.target.style.backgroundColor = customColorInput.value;
+  customColorInput.addEventListener('input', () => {
+    customColorRadio.checked = true;
+  })
+}
+function randomColor() {
+  const randomR = Math.floor(Math.random() * 256);
+  const randomG = Math.floor(Math.random() * 256);
+  const randomB = Math.floor(Math.random() * 256);
+  const randomA = Math.floor(Math.random() * 8 + 3);
+  event.target.style.backgroundColor = `rgba(${randomR}, ${randomG}, ${randomB}, 0.${randomA})`
+}
+function darkGradient() {
+  // not working yet
+}
+function lightGradient() {
+  // not working yet
+}
+function eraser() {
+  event.target.style.backgroundColor = canvasColorInput.value;
+}
 
-// Clear whole grid:
-// 1) Remove all grid elements
-// 2) Add new grid elements according to grid range input
-clearFieldBtn.addEventListener('click', function() {
-  emptyGrid()
-  setGrid(gridSizerInput.value)
+
+// Canvas options
+canvasColorInput.addEventListener('input', () => {
+  gridContainer.style.backgroundColor = canvasColorInput.value;
+  canvasColorRadio.checked = true;
 })
+// show grid option
 
 
 let showGridBtnActive = false;
 showGridBtn.addEventListener('click', function() {
   showGridBtn.classList.toggle('btn-active');
-  if (showGridBtnActive === false) {
-    showGridBtnActive = true;
-  } else if (showGridBtnActive === true) {
-    showGridBtnActive = false;
-  }
+  showGridBtnActive === false ? showGridBtnActive = true : false;
 })
 
 
-// Set grid container template according to input
+gridInputRange.addEventListener('input', () => {
+  setGrid(gridInputRange.value);
+  gridSizerDescription.innerHTML = `Grid size ${gridInputRange.value} x ${gridInputRange.value}`;
+})
+clearCanvasBtn.addEventListener('click', () => {
+  clearCanvas();
+  setGrid(gridInputRange.value);
+})
+
+
+// Sub functions
+function clearCanvas() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild)
+  }
+}
 function setGrid(value) {
   gridContainer.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${value}, 1fr)`;
-
-  // Create grid elements according to input
-  // Toggle grid on showGridBtn (de)activation
-  // Sensetize grid elements to Draw
-  // Append grid elements to grid container
   for (let i = 0; i < value * value; i++) {
-    const gridElement = document.createElement('div');
-
-    // showGridBtn.addEventListener('click', function() {
-    //   showGridBtn.classList.toggle('btn-active');
-    //   gridElement.classList.toggle('grid__element');
-    // })
-
-    if (showGridBtnActive === true) {
-      gridElement.classList.add('grid__element');
-    } else if (showGridBtnActive === false) {
-      gridElement.classList.remove('grid__element');
-
-    }
-
+    let gridElement = document.createElement('div');
     gridElement.addEventListener('mouseover', draw);
     gridElement.addEventListener('mousedown', draw);
     gridContainer.appendChild(gridElement);
   }
 }
-
-
-
-// Remove all children grid container
-function emptyGrid() {
-  while (gridContainer.firstChild) {
-    gridContainer.removeChild(gridContainer.firstChild)
-  }
-}
-
-
-
-// Add color to targetted grid element only when mouse is is pressed down
-function draw(event) {
-  if (event.type === 'mouseover' && !mouseIsDown) return
-  event.target.style.backgroundColor = customColorInput.value;
-  if (eraserBtnActive === true) {
-    event.target.style.backgroundColor = '';
-  } else if (randomColorInputActive === true) {
-    const randomR = Math.floor(Math.random() * 256);
-    const randomG = Math.floor(Math.random() * 256);
-    const randomB = Math.floor(Math.random() * 256);
-    const randomA = Math.floor(Math.random() * 8 + 3);
-    event.target.style.backgroundColor = `rgba(${randomR}, ${randomG}, ${randomB}, 0.${randomA})`
-  }
-}
-
-
-
-// Set grid background according to input
-canvasColorInput.addEventListener('input', () => gridContainer.style.backgroundColor = canvasColorInput.value)
-
-
-
-// On input grid size range:
-// Remove all children grid container
-// Add children to grid container according newly specified range input
-// Update grid range description according newly specified range input
-gridSizerInput.addEventListener('input', function() {
-  emptyGrid()
-  setGrid(gridSizerInput.value)
-  gridSizerDescription.innerHTML = `Grid size ${gridSizerInput.value} x ${gridSizerInput.value}`;
-})
